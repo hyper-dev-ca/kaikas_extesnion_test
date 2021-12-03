2414148c1b35d64131e353ae4249514cf455889d
Johnny <johnny.na@groundx.xyz>
2021-12-03 17:11:03 +0900

    최초 세팅

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
+import Caver from 'caver-js'
 import logo from './logo.svg';
 import './App.css';
 
+const caver = new Caver(window.klaytn)
+
 function App() {
+
+  async function testAction() {
+    const accounts = await window.klaytn.enable()
+    const account = accounts[0]
+    const selectedAddress = window.klaytn.selectedAddress
+    console.log(account)
+    console.log(window.klaytn.networkVersion)
+    console.log(window.klaytn.selectedAddress)
+    console.log(caver.version)
+
+    const kip7Instance = new caver.klay.KIP7('0x6117FAfa0ef66eEEA30e33f8B810ABF65488A393')
+    kip7Instance.transfer('0xf165d894b980f3270677e5857fead95154925b2a',
+        caver.utils.toPeb(1, 'KLAY'),
+        { from: selectedAddress }).then(console.log)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={
+          testAction
+        }>
+          Button
+        </button>
       </header>
     </div>
   );
