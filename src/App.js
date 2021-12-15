a88b71a9f4df18d300d996d2ab61883bd5ecd5ca
dev-jiwon <devjiwon@gmail.com>
2021-12-15 09:33:47 +0900

    AccountsChange, networkChanged Test

--- src/App.js
+++ src/App.js
@@ -8,17 +8,25 @@ function App() {
 
   async function testAction() {
     const accounts = await window.klaytn.enable()
-    const account = accounts[0]
-    const selectedAddress = window.klaytn.selectedAddress
-    console.log(account)
-    console.log(window.klaytn.networkVersion)
-    console.log(window.klaytn.selectedAddress)
-    console.log(caver.version)
+    // const account = accounts[0]
+    // const selectedAddress = window.klaytn.selectedAddress
+    // console.log(account)
+    // console.log(window.klaytn.networkVersion)
+    // console.log(window.klaytn.selectedAddress)
+    // console.log(caver.version)
 
-    const kip7Instance = new caver.klay.KIP7('0x6117FAfa0ef66eEEA30e33f8B810ABF65488A393')
-    kip7Instance.transfer('0xf165d894b980f3270677e5857fead95154925b2a',
-        caver.utils.toPeb(1, 'KLAY'),
-        { from: selectedAddress }).then(console.log)
+    // const kip7Instance = new caver.klay.KIP7('0x6117FAfa0ef66eEEA30e33f8B810ABF65488A393')
+    // kip7Instance.transfer('0xf165d894b980f3270677e5857fead95154925b2a',
+    //     caver.utils.toPeb(1, 'KLAY'),
+    //     { from: selectedAddress }).then(console.log)
+
+    window.klaytn.on('accountsChanged', function(accounts) {
+      console.log(window.klaytn.selectedAddress)
+    })
+
+    window.klaytn.on('networkChanged', function() {
+      console.log(window.klaytn.networkVersion)
+    })
   }
 
   return (
